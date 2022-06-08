9eafb8e41a65ece6184100d792bf1f588ea93856
boryand <boryand>
2022-06-08 19:32:03 +0200

    some changes with the media

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,3 @@
-import video from "../media/video.mp4";
 import logo from "../media/logo.png";
 
 const Landing = () => {
@@ -6,7 +5,10 @@ const Landing = () => {
     <div className="container1">
       <h1 className="message"> Enjoy the lux , you deserve it !</h1>
       <video width="1500" height="500" className="vid" controls>
-        <source src={video} type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/bobchi/video/upload/v1654709462/video_ievzxu.mp4"
+          type="video/mp4"
+        />
       </video>
     </div>
   );
