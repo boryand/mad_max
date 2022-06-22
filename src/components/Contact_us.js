6eae9a743284223f56c0f3e13eb29d367adff947
boryand <boryand>
2022-06-22 11:28:21 +0200

    some changes

--- src/components/Contact_us.js
+++ src/components/Contact_us.js
@@ -12,18 +12,21 @@ const Contact_us = () => {
         <h4 className="my_contacts"> Contact the developer</h4>
         <h6> Boryana Dimitrova</h6>
 
-        <a href="mailto:boryana.dimitrova789@gmail.com">
+        <a href="mailto:boryana.dimitrova789@gmail.com" target="_blank">
           <img alt="media" src={social1} className="media" />
         </a>
-        <a href="http://www.linkedin.com/in/boryana-dimitrova94/">
+        <a
+          href="http://www.linkedin.com/in/boryana-dimitrova94/"
+          target="_blank"
+        >
           {" "}
           <img alt="media" src={social2} className="media" />{" "}
         </a>
-        <a href="https://github.com/boryand">
+        <a href="https://github.com/boryand" target="_blank">
           {" "}
           <img alt="media" src={social3} className="media" />{" "}
         </a>
-        <a href="https://boryana-portfolio.herokuapp.com/">
+        <a href="https://www.boryana-dimitrova.com/" target="_blank">
           {" "}
           <img alt="media" src={web_icon} className="media" />{" "}
         </a>
