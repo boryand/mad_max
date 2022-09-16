b3ffcab1300f6f97ab89cfcc9e293bece67d9900
boryand <boryand>
2022-09-16 12:36:12 +0200

    fixing some bugs

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@
 const Landing = () => {
   return (
     <div className="container1">
-      <h1 className="message"> Enjoy the lux , you deserve it !</h1>
+      <h1 className="message"> Enjoy the lux , you deserve it !!!</h1>
       <video width="1500" height="500" className="vid" controls>
         <source
           src="https://res.cloudinary.com/bobchi/video/upload/v1654709462/video_ievzxu.mp4"
